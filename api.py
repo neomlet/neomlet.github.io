248dea32fee38cc6d129a2f4ffeb14fbea80e065
人神 <100081529+neomlet@users.noreply.github.com>
2025-02-09 14:39:23 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from storage import activity_storage
+import os
 
 app = Flask(__name__)
 CORS(app)  # Включаем CORS
@@ -17,4 +18,5 @@ def get_activity():
     return jsonify({"error": "Данные не найдены"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Используем порт из переменной окружения
+    app.run(host="0.0.0.0", port=port)
